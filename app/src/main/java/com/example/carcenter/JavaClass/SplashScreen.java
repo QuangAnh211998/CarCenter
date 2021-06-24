c2e502ba4f774e582c4acdd0025f8e13370dafb1
QuangAnh211998 <voquanganh12@gmail.com>
2021-06-24 23:53:32 +0700

    Display the SplashScreen

--- app/src/main/java/com/example/carcenter/JavaClass/SplashScreen.java
+++ app/src/main/java/com/example/carcenter/JavaClass/SplashScreen.java
@@ -3,8 +3,10 @@ package com.example.carcenter.JavaClass;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -12,19 +14,24 @@ import androidx.core.content.ContextCompat;
 import com.example.carcenter.R;
 
 public class SplashScreen extends AppCompatActivity {
+    private static int Splash_screen = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
-
-        if(Build.VERSION.SDK_INT>=22){
+        if (Build.VERSION.SDK_INT >= 22) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            getWindow().setStatusBarColor(ContextCompat.getColor(SplashScreen.this,R.color.colorGrey));
+            getWindow().setStatusBarColor(ContextCompat.getColor(SplashScreen.this, R.color.colorGrey));
         }
-        //sleep 3 seconds
-        SystemClock.sleep(1000);
-        startActivity(new Intent(getApplication(), MainActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplication(), MainActivity.class));
+            }
+        }, Splash_screen);
+
     }
 
 }
